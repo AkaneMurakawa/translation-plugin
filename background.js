0b1610cae241865539a9953991956f96107fe5d8
AkaneMurakawa <chenshjing@gmail.com>
2023-04-23 23:28:52 +0800

    代码整理

--- background.js
+++ background.js
@@ -2,14 +2,16 @@
 document.addEventListener('mouseup', selectWords, true);
 
 // get the select words
-function selectWords(event){
+function selectWords(event) {
     var selection = window.getSelection();
     var text = selection.toString();
-    if (text === '' || !text){
+    if (text === '' || !text) {
         return;
     }
 
-    chrome.storage.sync.set({'text': text}, function(){
+    chrome.storage.sync.set({
+        'text': text
+    }, function () {
         console.log('select content: ', text)
     });
 }
